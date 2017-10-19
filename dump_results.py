8a2ea6a5d0916236820a711e37790cab317eb4aa
Iain Bryson <iain@iain-bryson.ca>
2017-10-18 21:01:30 -0700

    Add test code to dump study results.

---
+++ dump_results.py
@@ -0,0 +1,63 @@
+import xml.etree.cElementTree as ET
+import json
+from collections import defaultdict
+import pprint
+from pyswagger import App, Security
+from pyswagger.contrib.client.requests import Client
+from pyswagger.utils import jp_compose
+import sys
+
+pp = pprint.PrettyPrinter(indent=4)
+
+public_client_id = 'admin_public'
+public_client_secret = 'czZCaGRSa3F0MzpnWDFmQmF0M2JW'
+
+app = App._create_('http://localhost:3000/apidocs/v1/swagger.json')
+# init swagger client
+client = Client(Security(app))
+
+# a request to create a new pet
+auth_request=dict(client_id=public_client_id,
+                  client_secret=public_client_secret,
+                  grant_type='password',
+                  email='admin@elicit.com',
+                  password='password') 
+resp = client.request(app.op['getAuthToken'](auth_request=auth_request))
+
+assert resp.status == 200
+
+access_token = resp.data.access_token
+auth = 'Bearer ' + access_token
+
+resp = client.request(app.op['findStudyResults'](authorization=auth, study_definition_id=1))
+
+assert resp.status == 200
+
+pp.pprint(resp.data)
+
+
+resp = client.request(app.op['findExperiments'](authorization=auth, study_definition_id=1, protocol_definition_id=1))
+
+assert resp.status == 200
+
+pp.pprint(resp.data)
+
+
+resp = client.request(app.op['findStages'](authorization=auth, study_definition_id=1, protocol_definition_id=1, phase_definition_id=1))
+
+assert resp.status == 200
+
+pp.pprint(resp.data)
+
+
+
+resp = client.request(app.op['findDataPoints'](authorization=auth, study_definition_id=1, protocol_definition_id=1, phase_definition_id=1, trial_definition_id=1))
+
+assert resp.status == 200
+
+pp.pprint(resp.data)
+
+
+resp = client.request(app.op['queryDataPoints'](authorization=auth, study_definition_id=1, protocol_definition_id=1))
+assert resp.status == 200
+pp.pprint(resp.data)
