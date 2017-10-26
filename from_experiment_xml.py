1a4e48d5032cd3d616ad7140791b08ee1c330117
Iain Bryson <iain@iain-bryson.ca>
2017-10-26 09:44:10 -0700

    Refactor script to make it cleaner

--- from_experiment_xml.py
+++ from_experiment_xml.py
@@ -10,60 +10,10 @@ import sys
 # usage:
 # find . -iname "*.xml" -exec python from_experiment_xml.py {} \;
 
-pp = pprint.PrettyPrinter(indent=4)
 
-public_client_id = 'admin_public'
-public_client_secret = 'czZCaGRSa3F0MzpnWDFmQmF0M2JW'
-
-# load Swagger resource file into App object
-app = App._create_('http://localhost:3000/apidocs/v1/swagger.json')
-
-auth = Security(app)
-#auth.update_with('api_key', 'admin_public') # api key
-#auth.update_with('petstore_auth', 'czZCaGRSa3F0MzpnWDFmQmF0M2JW') # oauth2
-
-# init swagger client
-client = Client(auth)
-
-# a request to create a new pet
-auth_request=dict(client_id=public_client_id,
-                  client_secret=public_client_secret,
-                  grant_type='password',
-                  email='admin@elicit.com',
-                  password='password') 
-print(auth_request)
-# making a request
-resp = client.request(app.op['getAuthToken'](auth_request=auth_request))
-
-assert resp.status == 200
-
-
-#
-# Add access token 
-#
-
-access_token = resp.data.access_token
-pp.pprint(resp.data)
-auth = 'Bearer ' + access_token
-client._Client__s.headers['Authorization'] = auth
-
-
-resp = client.request(app.op['getCurrentUser'](authorization=auth))
-
-assert resp.status == 200
-
-print("Current User:")
-pp.pprint(resp.data)
-
-user = resp.data
-
-
-resp = client.request(app.op['findUsers'](authorization=auth))
-
-assert resp.status == 200
-
-registered_users = list(filter(lambda x: x.role == 'registered_user', resp.data))
-
+##
+## UTILITIES
+##
 
 # https://stackoverflow.com/questions/7684333/converting-xml-to-dictionary-using-elementtree
 def etree_to_dict(t):
@@ -112,6 +62,39 @@ class ComponentParser:
         return self.do_default(element)
 
 
+##
+## MAIN
+##
+
+pp = pprint.PrettyPrinter(indent=4)
+
+public_client_id = 'admin_public'
+public_client_secret = 'czZCaGRSa3F0MzpnWDFmQmF0M2JW'
+admin_user = 'admin@elicit.dk'
+admin_password = 'password'
+
+# if you try this with a non-admin user, you get a fail
+#admin_user = 'subject1@elicit.com'
+#admin_password = 'abcd12_'
+
+
+# load Swagger resource file into App object
+app = App._create_('http://localhost:3000/apidocs/v1/swagger.json')
+#app = App._create_('http://docker.local/apidocs/v1/swagger.json')
+#app = App._create_('http://elicit.compute.dtu.dk/apidocs/v1/swagger.json')
+
+auth = Security(app)
+#auth.update_with('api_key', 'admin_public') # api key
+#auth.update_with('petstore_auth', 'czZCaGRSa3F0MzpnWDFmQmF0M2JW') # oauth2
+
+# init swagger client
+client = Client(auth)
+
+
+#
+# Parse command line and load XML tree
+#
+
 if len(sys.argv) >= 2:
   file = sys.argv[1]
 else:
@@ -121,6 +104,56 @@ tree = ET.ElementTree(file=file)
 
 root = tree.getroot() # experiment
 root_tags = list(map(lambda x: x.tag, root))
+
+
+#
+# Login admin user to create study
+#
+auth_request=dict(client_id=public_client_id,
+                  client_secret=public_client_secret,
+                  grant_type='password',
+                  email=admin_user,
+                  password=admin_password) 
+resp = client.request(app.op['getAuthToken'](auth_request=auth_request))
+
+assert resp.status == 200
+
+
+#
+# Add access token 
+#
+
+access_token = resp.data.access_token
+auth = 'Bearer ' + access_token
+client._Client__s.headers['Authorization'] = auth
+
+#
+# Double-check that we have the right user
+#
+
+resp = client.request(app.op['getCurrentUser'](authorization=auth))
+
+assert resp.status == 200
+
+print("Current User:")
+pp.pprint(resp.data)
+
+user = resp.data
+
+
+#
+# Get list of users who will use the study
+#
+
+resp = client.request(app.op['findUsers'](authorization=auth))
+
+assert resp.status == 200
+
+registered_users = list(filter(lambda x: x.role == 'registered_user', resp.data))
+
+#
+# Create Study
+#
 
 study_definition = dict(title=root[root_tags.index('Name')].text,
                         description=root[root_tags.index('ExperimentDescription')].text,
@@ -136,18 +169,22 @@ study_definition = dict(title=root[root_tags.index('Name')].text,
 new_study = dict(study_definition=study_definition)
 resp = client.request(app.op['addStudy'](authorization=auth, study=new_study))
 
+if resp.status != 201:
+  print("Failed to create study!")
+  pp.pprint(resp.data)
 assert resp.status == 201
 
 new_study = resp.data
-
-print(new_study)
 
 #
 # Add a new Protocol Definition
 #
 
-new_protocol_definition = dict(protocol_definition=dict(name='Newly created protocol definition from Python', definition_data="foo"))
-resp = client.request(app.op['addProtocolDefinition'](authorization=auth, protocol_definition=new_protocol_definition, study_definition_id=new_study.id))
+new_protocol_definition = dict(protocol_definition=dict(name='Newly created protocol definition from Python',
+                                                        definition_data="foo"))
+resp = client.request(app.op['addProtocolDefinition'](authorization=auth,
+                                                      protocol_definition=new_protocol_definition,
+                                                      study_definition_id=new_study.id))
 
 assert resp.status == 201
 
@@ -161,9 +198,13 @@ print(new_protocol_definition)
 
 for user in registered_users:
   pp.pprint(user)
-  protocol_user = dict(protocol_user=dict(user_id=user.id, study_definition_id=new_study.id, protocol_definition_id=new_protocol_definition.id))
-#study_definition_id=new_study.id,   
-  resp = client.request(app.op['addProtocolUser'](authorization=auth, protocol_user=protocol_user, study_definition_id=new_study.id, protocol_definition_id=new_protocol_definition.id))
+  protocol_user = dict(protocol_user=dict(user_id=user.id,
+                                          study_definition_id=new_study.id,
+                                          protocol_definition_id=new_protocol_definition.id))
+  resp = client.request(app.op['addProtocolUser'](authorization=auth,
+                                                  protocol_user=protocol_user,
+                                                  study_definition_id=new_study.id,
+                                                  protocol_definition_id=new_protocol_definition.id))
 
   assert resp.status == 201
 
@@ -171,27 +212,32 @@ for user in registered_users:
 # Add a new Phase Definition
 #
 
-new_phase_definition = dict(phase_definition=dict(name='Newly created phase definition from Python', definition_data="foo"))
-resp = client.request(app.op['addPhaseDefinition'](authorization=auth, phase_definition=new_phase_definition, study_definition_id=new_study.id, protocol_definition_id=new_protocol_definition.id))
+new_phase_definition = dict(phase_definition=dict(name='Newly created phase definition from Python',
+                                                  definition_data="foo"))
+resp = client.request(app.op['addPhaseDefinition'](authorization=auth,
+                                                   phase_definition=new_phase_definition,
+                                                   study_definition_id=new_study.id,
+                                                   protocol_definition_id=new_protocol_definition.id))
 
 assert resp.status == 201
 
 new_phase_definition = resp.data
 
-print(new_phase_definition)
-
 #
 # Add a new Phase Order
 #
 
-new_phase_order = dict(phase_order=dict(name='Newly created phase order from Python', sequence_data="0", user_id=0))
-resp = client.request(app.op['addPhaseOrder'](authorization=auth, phase_order=new_phase_order, study_definition_id=new_study.id, protocol_definition_id=new_protocol_definition.id))
+new_phase_order = dict(phase_order=dict(name='Newly created phase order from Python',
+                                        sequence_data="0",
+                                        user_id=0))
+resp = client.request(app.op['addPhaseOrder'](authorization=auth,
+                                              phase_order=new_phase_order,
+                                              study_definition_id=new_study.id,
+                                              protocol_definition_id=new_protocol_definition.id))
 
 assert resp.status == 201
 
 new_phase_order = resp.data
-
-print(new_phase_order)
 
 trials = root[root_tags.index('Trials')]
 
@@ -200,8 +246,13 @@ for trial in trials:
   # Add a new Trial Definition
   #
 
-  new_trial_definition = dict(trial_definition=dict(name='Newly created trial definition from Python', definition_data="foo"))
-  resp = client.request(app.op['addTrialDefinition'](authorization=auth, trial_definition=new_trial_definition, study_definition_id=new_study.id, protocol_definition_id=new_protocol_definition.id, phase_definition_id=new_phase_definition.id))
+  new_trial_definition = dict(trial_definition=dict(name='Newly created trial definition from Python',
+                                                    definition_data="I DON't KNOW WHAT WILL GO HERE"))
+  resp = client.request(app.op['addTrialDefinition'](authorization=auth,
+                                                     trial_definition=new_trial_definition,
+                                                     study_definition_id=new_study.id,
+                                                     protocol_definition_id=new_protocol_definition.id,
+                                                     phase_definition_id=new_phase_definition.id))
 
   assert resp.status == 201
 
@@ -215,26 +266,36 @@ for trial in trials:
     # Add a new Component
     #
 
-    new_component = dict(component=dict(name='Newly created component definition from Python', definition_data=component_data))
-    resp = client.request(app.op['addComponent'](authorization=auth, component=new_component, study_definition_id=new_study.id, protocol_definition_id=new_protocol_definition.id, phase_definition_id=new_phase_definition.id, trial_definition_id=new_trial_definition.id))
+    new_component = dict(component=dict(name=("Newly created component definition  %s from Python"%component.tag),
+                                        definition_data=component_data))
+    resp = client.request(app.op['addComponent'](authorization=auth,
+                                                 component=new_component,
+                                                 study_definition_id=new_study.id,
+                                                 protocol_definition_id=new_protocol_definition.id,
+                                                 phase_definition_id=new_phase_definition.id,
+                                                 trial_definition_id=new_trial_definition.id))
 
     assert resp.status == 201
 
     new_component = resp.data
 
-    print(new_component)
+    #print(new_component)
 
 #
 # Add a new Trial Order
 #
 
-new_trial_order = dict(trial_order=dict(name='Newly created trial order from Python', sequence_data=",".join([str(x) for x in range(len(trials))]), user_id="0"))
-resp = client.request(app.op['addTrialOrder'](authorization=auth, trial_order=new_trial_order, study_definition_id=new_study.id, protocol_definition_id=new_protocol_definition.id, phase_definition_id=new_phase_definition.id))
+new_trial_order = dict(trial_order=dict(name='Newly created trial order from Python',
+                                        sequence_data=",".join([str(x) for x in range(len(trials))]),
+                                        user_id="0"))
+resp = client.request(app.op['addTrialOrder'](authorization=auth,
+                                              trial_order=new_trial_order,
+                                              study_definition_id=new_study.id,
+                                              protocol_definition_id=new_protocol_definition.id,
+                                              phase_definition_id=new_phase_definition.id))
 
 assert resp.status == 201
 
 new_trial_order = resp.data
-
-print(new_trial_order)
 
 
