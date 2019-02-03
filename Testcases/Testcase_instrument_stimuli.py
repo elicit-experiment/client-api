2fbe7b469ce7ea70d372f8d0f1ae446a23e045e1
Jens Madsen <jenma@dtu.dk>
2019-02-04 00:22:15 +0100

    Replace Testcase_instrument_stimuli.py

--- Testcases/Testcase_instrument_stimuli.py
+++ Testcases/Testcase_instrument_stimuli.py
@@ -13,6 +13,7 @@ import json
 
 from examples_base import parse_command_line_args
 from pyelicit import elicit
+from random import shuffle
 
 ## Stimuli URLs
 audio_url = "https://www.mfiles.co.uk/mp3-downloads/franz-liszt-liebestraum-3-easy-piano.mp3"
@@ -502,24 +503,36 @@ component_object = elicit_object.add_component(component=dict(component=componen
                                                phase_definition_id=phase_object.id,
                                                trial_definition_id=trial_object.id)
 
-#
-# Add a Trial Orders to the study
-#
+#%% Add Trial Orders to the study
 
-# Define the trial orders
+#trail_orders for specific users
 for study_participant in study_participants:
-    trial_order_specification = dict(trial_order=dict(sequence_data=",".join([str(trial.id) for trial in trials]),
-                                                   user_id=study_participant.id))
+    trial_order_specification_user = dict(trial_order=dict(sequence_data=",".join([str(trial.id) for trial in trials]),user_id=study_participant.id))
+    print(trial_order_specification_user)
+    
+    # Trial order addition
+    trial_order_object = elicit_object.add_trial_order(trial_order=trial_order_specification_user,
+                                                       study_definition_id=study_object.id,
+                                                       protocol_definition_id=protocol_object.id,
+                                                       phase_definition_id=phase_object.id)
+#trail_orders for anonymous users
+for anonymous_participant in range(0,10):
+    trial_id = [int(trial.id) for trial in trials]
+    shuffle(trial_id)
 
-# Trial order addition
-    trial_order_object = elicit_object.add_trial_order(trial_order=trial_order_specification,
-                                         study_definition_id=study_object.id,
-                                         protocol_definition_id=protocol_object.id,
-                                         phase_definition_id=phase_object.id)
+    trial_order_specification_anonymous = dict(trial_order=dict(sequence_data=",".join(map(str,trial_id))))
+    print(trial_order_specification_anonymous)
+    
+    # Trial order addition
+    trial_order_object = elicit_object.add_trial_order(trial_order=trial_order_specification_anonymous,
+                                                       study_definition_id=study_object.id,
+                                                       protocol_definition_id=protocol_object.id,
+                                                       phase_definition_id=phase_object.id)
+    
 
-#
-# Add a new Phase Order
-#
+
+#%% Add a new Phase Order
+
 phase_sequence_data = ",".join([str(phase_definition.id) for phase_definition in phases])
 
 phase_order_specification = dict(phase_order=dict(sequence_data=phase_sequence_data,
